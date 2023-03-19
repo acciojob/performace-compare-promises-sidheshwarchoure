ef600a0adfacd9a7ef29c229e4f72b1597418df9
sidheshwarchoure <92964594+sidheshwarchoure@users.noreply.github.com>
2023-03-19 16:12:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,36 @@ const apiUrls = [
 ];
 
 // You can write your code here
+const outputAll = document.getElementById('output-all');
+const outputAny = document.getElementById('output-any');
+
+function fetchData(url) {
+  return fetch(url).then(response => response.json());
+}
+
+function fetchAll() {
+  const start = performance.now();
+  Promise.all(apiUrls.map(fetchData))
+    .then(data => {
+      const end = performance.now();
+      const timeTaken = end - start;
+      outputAll.innerText = `${timeTaken} ms`;
+      console.log('Promise.all took', timeTaken, 'ms');
+    })
+    .catch(error => console.log(error));
+}
+
+function fetchAny() {
+  const start = performance.now();
+  Promise.any(apiUrls.map(fetchData))
+    .then(data => {
+      const end = performance.now();
+      const timeTaken = end - start;
+      outputAny.innerText = `${timeTaken} ms`;
+      console.log('Promise.any took', timeTaken, 'ms');
+    })
+    .catch(error => console.log(error));
+}
+
+fetchAll();
+fetchAny();
